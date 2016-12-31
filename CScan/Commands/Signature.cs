a2e4d8f96187a9706633a60a2932e01bfc4c2d09
Ian Carroll <ian@ian.sh>
2016-12-31 03:20:00 -0500

    Add support for :Signatures.
    This invokes the Authenticode class used throughout the application.

---
+++ CScan/Commands/Signature.cs
@@ -0,0 +1,38 @@
+﻿using CScan;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CScan.Commands
+{
+    internal class Signature : ICommand
+    {
+        public List<Dictionary<string, string>> Run(List<string> arguments, List<Dictionary<string, string>> list)
+        {
+            foreach (string file in arguments)
+            {
+                if (!File.Exists(file))
+                {
+                    list.Add(new Dictionary<string, string>
+                    {
+                        {"token", "Signature"},
+                        {"raw", "Could not find " + file },
+                    });
+
+                    continue;
+                }
+
+                bool isSigned = Authenticode.IsSigned(file);
+
+                list.Add(new Dictionary<string, string>
+                {
+                    {"token", "Signature"},
+                    {"raw", file + " is" + (isSigned ? " " : " not ") + "signed"},
+                });
+            }
+
+            return list;
+        }
+    }
+}
